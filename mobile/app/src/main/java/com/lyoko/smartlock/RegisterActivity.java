ec4394eeb22f472a360229bb9c891a5c258aa062
Phương Nguyễn <hphuong123123@gmail.com>
2020-02-08 22:05:15 +0700

    Hoàn thiện giao diện đăng kí SĐT/Email

--- mobile/app/src/main/java/com/lyoko/smartlock/RegisterActivity.java
+++ mobile/app/src/main/java/com/lyoko/smartlock/RegisterActivity.java
@@ -1,30 +1,78 @@
 package com.lyoko.smartlock;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private RadioGroup toggle;
-    private RadioButton rdSoDienThoai, rdEmail;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        toggle = findViewById(R.id.toggle);
-        rdSoDienThoai = findViewById(R.id.rdSoDienThoai);
-        rdEmail = findViewById(R.id.rdEmail);
+        Spinner spinner = findViewById(R.id.spiner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list_quoc_gia, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
-        rdSoDienThoai.setOnCheckedChangeListener(new RadioButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//TAB HOST
+        TabHost tabHost = findViewById(R.id.tabHost);
+        tabHost.setup();
+//TAB1
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("Tab1");
+        tab1.setIndicator("Số điện thoại");
+        tab1.setContent(R.id.tab1);
+//TAB2
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("Tab1");
+        tab2.setIndicator("Email");
+        tab2.setContent(R.id.tab2);
 
-            }
-        });
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+
+        ActionBar actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+//quay ve dang nhap
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
