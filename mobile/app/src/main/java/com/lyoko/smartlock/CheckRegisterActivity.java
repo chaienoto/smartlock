a723cb028b13dc7ee90c04b2ded5deee55a5da0d
Phương Nguyễn <hphuong123123@gmail.com>
2020-02-12 21:30:00 +0700

    Hoàn thành giao diện đăng kí và sừa đổi tên một số file

---
+++ mobile/app/src/main/java/com/lyoko/smartlock/CheckRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.lyoko.smartlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_dialog);
+    }
+}
