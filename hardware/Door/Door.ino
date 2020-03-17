58533918f7fcfbd0b1e833d210981bf37989810d
chaienoto <chaienoto098gmail.com>
2020-03-17 15:36:47 +0700

    add batteryService

--- hardware/Door/Door.ino
+++ hardware/Door/Door.ino
@@ -26,16 +26,19 @@
 #include <BLE2902.h>
 
 BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
+uint8_t level = 57;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define Battery_Service BLEUUID((uint16_t)0x180F)
+BLECharacteristic pBattery_Level (BLEUUID((uint16_t)0x2A19),
+                                  BLECharacteristic::PROPERTY_READ |
+                                  BLECharacteristic::PROPERTY_NOTIFY);
+
 
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -55,34 +58,28 @@ void setup() {
   Serial.begin(115200);
 
   // Create the BLE Device
-  BLEDevice::init("ESP32");
+  BLEDevice::init("Lyoko Smart Lock");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLEService *pBattery_Service = pServer->createService(Battery_Service);
 
   // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
+  pBattery_Service->addCharacteristic(&pBattery_Level);
 
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   // Create a BLE Descriptor
-  pCharacteristic->addDescriptor(new BLE2902());
+  pBattery_Level.addDescriptor(new BLE2902());
 
   // Start the service
-  pService->start();
+  pBattery_Service->start();
 
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->addServiceUUID(Battery_Service);
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
@@ -90,23 +87,36 @@ void setup() {
 }
 
 void loop() {
-    // notify changed value
-    if (deviceConnected) {
-        pCharacteristic->setValue((uint8_t*)&value, 4);
-        pCharacteristic->notify();
-        value++;
-        delay(10); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-    }
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
+//  // notify changed value 
+//  if (deviceConnected) {
+//    pCharacteristic->setValue((uint8_t*)&value, 4);
+//    pCharacteristic->notify();
+//    value++;
+//    delay(10); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+//  }
+//  // disconnecting
+//  if (!deviceConnected && oldDeviceConnected) {
+//    delay(500); // give the bluetooth stack the chance to get things ready
+//    pServer->startAdvertising(); // restart advertising
+//    Serial.println("start advertising");
+//    oldDeviceConnected = deviceConnected;
+//  }
+//  // connecting
+//  if (deviceConnected && !oldDeviceConnected) {
+//    // do stuff here on connecting
+//    oldDeviceConnected = deviceConnected;
+//  }
+
+  pBattery_Level.setValue(&level, 1);
+  pBattery_Level.notify();
+  delay(5000);
+
+  level++;
+  Serial.println(int(level));
+
+  if (int(level) == 100)
+    level = 0;
+
+
+
 }
